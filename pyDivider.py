e1b7ee4ca3fa6ae12fa7213a35fb7a274cb585b1
CTConsulting 7 <ctconsulting7@Irian-MacBook-Pro.local>
2018-03-14 15:36:46 -0600

    Added sequence to notify file created

--- pyDivider.py
+++ pyDivider.py
@@ -1,6 +1,10 @@
 import csv
 import math
+import os
+import time
 
+asciimoji = ('ᕦ( ͡° ͜ʖ ͡°)ᕤ', '╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ', '(╯°□°)╯︵ ┻━┻', 'ヽ(｀Д´)ﾉ')
+os.system('cls||clear')
 print('PyDivider Alpha0.1')
 while True:
     try:
@@ -9,24 +13,28 @@ while True:
         csvToDivide = open(csvLocation, newline = '', encoding='utf-8')
         break
     except IOError:
-        print('File not found, try again. (╯°□°)╯︵ ┻━┻')
+        print('File not found, try again. {0}'.format(asciimoji[2]))
     except ValueError:
-        print('Invalid number of rows, try again. ヽ(｀Д´)ﾉ')
+        print('Invalid number of rows, try again. {0}'.format(asciimoji[3]))
+
 nameCSV = csvLocation.split('.')[0]
 readHeader = csv.reader(csvToDivide)
 bodyLst = list() #List of elements
 head = list() #List of header
+
 for row in readHeader:
     if readHeader.line_num == 1:
         head.append(row)
     else:
         bodyLst.append(row)
+
 header = head[0]
-# if (header[0] == 'Id' or header[0] == 'ID'):
-#     header[0] = '"{0}"'.format(header[0])
 numFiles = math.ceil(int(len(bodyLst)) / rowsPerFile) #Number of files to create
-print('Working... ヽ(｀Д´)⊃━☆ﾟ. * ･ ｡ﾟ,')
+
 for element in range(0, numFiles):
+    os.system('cls||clear')
+    print('{} File...'.format(asciimoji[0]))
+    time.sleep(0.7)
     nameF = (nameCSV + 'Child {0}' + '.csv').format(element) #Automatic name
     with open(nameF, 'w') as fileCSV:
         fileWriter = csv.writer(fileCSV, quoting = csv.QUOTE_ALL, lineterminator='\n')
@@ -34,5 +42,10 @@ for element in range(0, numFiles):
         for row in range(0, rowsPerFile):
             if len(bodyLst) > 0:
                 fileWriter.writerow(bodyLst.pop())
+    os.system('cls||clear')
+    print('{} Created!'.format(asciimoji[1]))
+    time.sleep(0.9)
+
+os.system('cls||clear')
 print('Done. ＼(＾O＾)／')
 csvToDivide.close()
